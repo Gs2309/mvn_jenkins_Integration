19c0910f82190310d00ad0dcef531aa9f2efd0de
Gs2309 <sherrym2309@gmail.com>
2024-07-15 12:56:34 +0000

    maven-jenkinsCommit

---
+++ src/main/java/com/test/app/App.java
@@ -0,0 +1,13 @@
+package com.test.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Maven_World!!" );
+    }
+}
